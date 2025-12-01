ea9c0037fa7a92d10058cad666a6f1a2d8fccbf4
luizh1406 <lhenriqueoliveira140603@gmail.com>
2025-12-01 08:52:45 -0300

    sonar teste

--- jest.config.js
+++ jest.config.js
@@ -1,49 +1,24 @@
-
-/** @type {import('jest').Config} */
 module.exports = {
-  // Define a localização dos seus arquivos de teste
-  roots: ["<rootDir>/tests"],
+  roots: ["<rootDir>/tests"],
 
-  // Configurações de transformação para suportar JS/JSX/TS/TSX
-  transform: {
-    "^.+\\.[jt]sx?$": "babel-jest",
-  },
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
 
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-    "^../database.js$": "<rootDir>/pages/api/database.js"
-  },
+  testEnvironment: "jsdom",
 
-  // Backend não usa DOM, mas usamos jsdom para simular o ambiente de navegador para o frontend
-  testEnvironment: "jsdom",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "text", "clover"],
 
-  // --- CONFIGURAÇÕES DE COBERTURA ---
-  collectCoverage: true,
-  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!**/.next/**",
+    "!coverage/**",
+  ],
 
-  coverageReporters: ["lcov", "text", "clover"], 
-
-  // Arquivos dos quais coletar cobertura:
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}",
-    "pages/**/*.{js,jsx,ts,tsx}",
-    "pages/api/**/*.{js,jsx,ts,tsx}",
-    "src/utils/**/*.{js,jsx,ts,tsx}",
-  ],
-  
-  coveragePathIgnorePatterns: [
-    "/node_modules/", 
-    "src/styles/containers/containers.ts" 
-  ],
-
-  // Limites globais de cobertura
-  coverageThreshold: {
-    global: {
-      statements: 20, 
-      branches: 20,
-      functions: 5,
-      lines: 20
-    }
-  }
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+  ],
 };
